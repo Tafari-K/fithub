e4d24e9fc299ed9d107567050944f05cd579d3a0
Tafari-K <kirton.tafari@gmail.com>
2026-02-17 05:22:42 +0000

    DEBUG = True

--- fithub/settings.py
+++ fithub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vafp63#zh-(y6kc6flrd&5cm%@%4t#(gb^1#)%=(3_^n9a#yl5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
