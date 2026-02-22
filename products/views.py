34ad6194f9056e65dcfd4abf763007809a79663d
Tafari-K <kirton.tafari@gmail.com>
2026-02-22 21:51:08 +0000

    set up products app, models, admin fixes, global templates and base layout. update bug fixes table

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    """
+    Display all products
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/product_list.html', context)
+
+
+def product_detail(request, pk):
+    """
+    Display a single product
+    """
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_detail.html', context)
