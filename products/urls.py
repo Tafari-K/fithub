a5ac4593e7145f30630b0f7a9c533103307a5797
Tafari-K <kirton.tafari@gmail.com>
2026-02-23 17:32:31 +0000

    created form for reviewing products, updated appropriate views, urls and templates

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ReviewCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-from .models import Product
+from django.views.generic import ListView, DetailView, CreateView
+
+from products.forms import ReviewForm
+from .models import Product, Review
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 
 
 def product_list(request):
@@ -40,3 +44,22 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "products/product_detail.html"
     context_object_name = "product"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ReviewForm()
+        return context
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewForm
+
+    def form_valid(self, form):
+        product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        form.instance.product = product
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('product_detail', kwargs={'pk': self.kwargs['pk']})
