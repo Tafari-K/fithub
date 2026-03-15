cb1f004a7421052bcb11251b0c4339292844698e
Tafari-K <kirton.tafari@gmail.com>
2026-03-15 19:02:24 +0000

    Improved checkout admin display for orders and order items

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'order_total', 'date')
+    ordering = ('-date',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'lineitem_total')
