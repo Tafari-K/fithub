0f972497f3808e3e80a96dbabd3b06b5c311cd59
Tafari-K <kirton.tafari@gmail.com>
2026-03-16 22:08:01 +0000

    Added edit and delete functionality for comments

--- community/urls.py
+++ community/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
     path('comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('comment/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
