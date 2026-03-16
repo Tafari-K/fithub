aa83c388a393b32c4c7bdff478759a57f6297a81
Tafari-K <kirton.tafari@gmail.com>
2026-03-16 21:28:40 +0000

    Added edit and delete functionality for community posts

--- community/urls.py
+++ community/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.community_home, name='community_home'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -7,12 +8,15 @@ def community_home(request):
     posts = Post.objects.all()
 
     if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('community_home')
+        if request.user.is_authenticated:
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('community_home')
+        else:
+            return redirect('account_login')
     else:
         form = PostForm()
 
@@ -22,3 +26,29 @@ def community_home(request):
     }
 
     return render(request, 'community/community_home.html', context)
+
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('community_home')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'community/edit_post.html', {'form': form, 'post': post})
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+
+    if request.method == "POST":
+        post.delete()
+        return redirect('community_home')
+
+    return render(request, 'community/delete_post.html', {'post': post})
