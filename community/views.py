26aaae5463b7e8b8f7380ef0c47af1921084569d
Tafari-K <kirton.tafari@gmail.com>
2026-03-16 21:24:20 +0000

    Added community post creation form

--- community/views.py
+++ community/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+
 
 def community_home(request):
     posts = Post.objects.all()
-    return render(request, 'community/community_home.html', {'posts': posts})
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('community_home')
+    else:
+        form = PostForm()
+
+    context = {
+        'posts': posts,
+        'form': form
+    }
+
+    return render(request, 'community/community_home.html', context)
