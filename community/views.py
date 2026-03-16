41c03e2dda898e231a330cfcd41dff9bd53d5abe
Tafari-K <kirton.tafari@gmail.com>
2026-03-16 21:21:42 +0000

    Added community post model and display posts

--- community/views.py
+++ community/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 
 def community_home(request):
-    return render(request, 'community/community_home.html')
+    posts = Post.objects.all()
+    return render(request, 'community/community_home.html', {'posts': posts})
