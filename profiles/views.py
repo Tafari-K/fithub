18b4ac1198204783bac3bdc7d4b1cab9e7557151
Tafari-K <kirton.tafari@gmail.com>
2026-03-16 17:34:33 +0000

    Added order history to profile page and updated profile view logic to retrieve user orders

--- profiles/views.py
+++ profiles/views.py
@@ -4,18 +4,29 @@ from django.shortcuts import render
 
 from .forms import UserProfileForm
 from .models import UserProfile
+from checkout.models import Order
 
 
 @login_required
 def profile(request):
+
     profile, created = UserProfile.objects.get_or_create(user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
+
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully.')
+
     else:
         form = UserProfileForm(instance=profile)
 
-    return render(request, 'profiles/profile.html', {'form': form})
+    orders = Order.objects.filter(email=request.user.email).order_by('-date')
+
+    context = {
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, 'profiles/profile.html', context)
