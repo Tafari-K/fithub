650f60b69a1f0b1319f9bd642f6aec5e09f5750b
Tafari-K <kirton.tafari@gmail.com>
2026-05-27 10:52:41 +0100

    Configure SMTP email backend

--- fithub/settings.py
+++ fithub/settings.py
@@ -149,7 +149,13 @@ ACCOUNT_SIGNUP_FIELDS = [
     'password2*'
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
